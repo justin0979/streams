a5cdc0c9d401eda91f01e8b636fe9e7372c0dc0b
justin0979 <justinm0979@gmail.com>
2021-08-31 17:28:36 -0500

    setup Router use

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,13 +1,33 @@
-import { useAppSelector } from '&hooks';
+import { Router, Route, Link } from 'react-router-dom';
+import history from '&src/history';
 
-const App: React.FC = () => {
-  const erase = useAppSelector((state) => state.erase);
-
+const pageOne = () => {
   return (
     <div>
-      <h1>App</h1>
-      {erase}
+      <h1>Page One</h1>
+      <Link to="/two">Page Two</Link>
     </div>
+  );
+};
+
+const pageTwo = () => {
+  return (
+    <div>
+      <h1>Page Two</h1>
+      <Link to="/">Page One</Link>
+    </div>
+  );
+};
+
+const App: React.FC = () => {
+  return (
+    <Router history={history}>
+      <div>
+        <h1>App</h1>
+        <Route path="/" exact component={pageOne} />
+        <Route path="/two" exact component={pageTwo} />
+      </div>
+    </Router>
   );
 };
 
