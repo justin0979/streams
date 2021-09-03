fab4f4068c6ba6afd74e73483f68fba930cc29d0
justin0979 <justinm0979@gmail.com>
2021-09-03 17:26:38 -0500

    install @types/gapi and @types/gapi.auth2

--- client/src/components/GoogleAuth.tsx
+++ client/src/components/GoogleAuth.tsx
@@ -1,13 +1,19 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const GoogleAuth: React.FC = () => {
+  const [auth, setAuth] = useState<any>();
+
   useEffect(() => {
     window.gapi.load('client.auth2', () => {
-      window.gapi.client.init({
-        clientId:
-          '42657505038-nbv58rj93heokrj5539l6ljm7kofc9dd.apps.googleusercontent.com',
-        scope: 'email',
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            '42657505038-nbv58rj93heokrj5539l6ljm7kofc9dd.apps.googleusercontent.com',
+          scope: 'email',
+        })
+        .then(() => {
+          setAuth(window.gapi.auth2.getAuthInstance());
+        });
     });
   }, []);
 
