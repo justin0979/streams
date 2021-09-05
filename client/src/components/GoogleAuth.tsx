84668a3ecdeb4d0736232bee1f8be722a7268727
justin0979 <justinm0979@gmail.com>
2021-09-05 10:59:40 -0500

    setup live updates of authentication status

--- client/src/components/GoogleAuth.tsx
+++ client/src/components/GoogleAuth.tsx
@@ -5,6 +5,7 @@ interface GoogleAuthProps {
 }
 
 const GoogleAuth: React.FC<GoogleAuthProps> = (props) => {
+  let auth: gapi.auth2.GoogleAuth;
   const [isSignedIn, setIsSignedIn] = useState<boolean | null>(
     null,
   );
@@ -20,11 +21,16 @@ const GoogleAuth: React.FC<GoogleAuthProps> = (props) => {
           scope: 'email',
         })
         .then(() => {
-          const auth = window.gapi.auth2.getAuthInstance();
+          auth = window.gapi.auth2.getAuthInstance();
           setIsSignedIn(auth.isSignedIn.get());
+          auth.isSignedIn.listen(onAuthChange);
         });
     });
   }, []);
+
+  const onAuthChange = () => {
+    setIsSignedIn(auth.isSignedIn.get());
+  };
 
   const renderAuthButton = (): JSX.Element => {
     if (isSignedIn === null) {
