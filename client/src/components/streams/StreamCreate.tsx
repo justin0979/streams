96b1a8c119b9bb527946654b31bf50d5f2f92c58
justin0979 <justinm0979@gmail.com>
2021-09-17 14:27:08 -0500

    refactor StreamCreate to display message when input is touched

--- client/src/components/streams/StreamCreate.tsx
+++ client/src/components/streams/StreamCreate.tsx
@@ -2,6 +2,7 @@ import { useCallback } from 'react';
 import {
   Field,
   WrappedFieldProps,
+  WrappedFieldMetaProps,
   InjectedFormProps,
   reduxForm,
   FormSubmitHandler,
@@ -11,13 +12,36 @@ interface CustomFormProps extends WrappedFieldProps {
   label?: string;
 }
 
+interface ValidateFormValues {
+  title?: string;
+  description?: string;
+}
+
 const StreamCreate: React.FC<InjectedFormProps> = (props) => {
+  const renderError = ({
+    error,
+    touched,
+  }: WrappedFieldMetaProps): JSX.Element | undefined => {
+    if (touched && error) {
+      return (
+        <div className="message message__error">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  };
+
   const renderInput = useCallback(
     (formProps: CustomFormProps) => {
       return (
         <div className="form__field">
           <label>{formProps.label}</label>
-          <input type="text" {...formProps.input} />
+          <input
+            className="input"
+            {...formProps.input}
+            autoComplete="off"
+          />
+          {renderError(formProps.meta)}
         </div>
       );
     },
@@ -48,6 +72,22 @@ const StreamCreate: React.FC<InjectedFormProps> = (props) => {
   );
 };
 
+const validate = (
+  formValues: ValidateFormValues,
+): ValidateFormValues => {
+  const errors: ValidateFormValues = {};
+
+  if (!formValues.title) {
+    errors.title = 'You must enter a title';
+  }
+  if (!formValues.description) {
+    errors.description = 'You must enter a description';
+  }
+
+  return errors;
+};
+
 export default reduxForm({
   form: 'streamCreate',
+  validate: validate,
 })(StreamCreate);
