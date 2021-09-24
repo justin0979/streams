1d90c8c5934ad360b454dab871eef6d3486fa700
justin0979 <justinm0979@gmail.com>
2021-09-24 18:31:40 -0500

    define REST action types

--- client/src/state/action-creators/index.ts
+++ client/src/state/action-creators/index.ts
@@ -1,5 +1,5 @@
-import { Dispatch } from 'redux';
-import { AxiosResponse } from 'axios';
+import {Dispatch} from 'redux';
+import {AxiosResponse} from 'axios';
 import {
   AuthActionType,
   RestActionType,
@@ -10,7 +10,7 @@ import {
   Action,
 } from '&state/actions';
 import streams from '&apis/streams';
-import { FormValuesType } from '&src/formValuesTypes';
+import {FormValuesType} from '&src/formValuesTypes';
 
 export const signIn = (
   userId: string | undefined,
@@ -30,15 +30,15 @@ export const signOut = (): SignOutAction => {
 
 export const createStream =
   (formValues: FormValuesType) =>
-  async (dispatch: Dispatch<Action>) => {
-    const response: AxiosResponse<FormValuesType> =
-      await streams.post('/streams', formValues);
+    async (dispatch: Dispatch<Action>) => {
+      const response: AxiosResponse<FormValuesType> =
+        await streams.post('/streams', formValues);
 
-    dispatch({
-      type: RestActionType.CREATE_STREAM,
-      payload: response.data,
-    });
-  };
+      dispatch({
+        type: RestActionType.CREATE_STREAM,
+        payload: response.data,
+      });
+    };
 
 export const fetchStreams =
   () => async (dispatch: Dispatch<Action>) => {
@@ -49,3 +49,30 @@ export const fetchStreams =
       payload: response.data,
     });
   };
+
+export const fetchStream = (id: number) => async (dispatch: Dispatch<Action>) => {
+  const response = await streams.get(`/streams/${id}`)
+
+  dispatch({
+    type: RestActionType.FETCH_STREAM,
+    payload: response.data
+  })
+}
+
+export const editStream = (id: number, formValues: FormValuesType) => async (dispatch: Dispatch<Action>) => {
+  const response = await streams.put(`/streams/${id}`, formValues)
+
+  dispatch({
+    type: RestActionType.EDIT_STREAM,
+    payload: response.data
+  })
+}
+
+export const deleteStream = (id: number) => async (dispatch: Dispatch<Action>) => {
+  await streams.delete(`/streams/${id}`)
+
+  dispatch({
+    type: RestActionType.DELETE_STREAM,
+    payload: id
+  })
+}
